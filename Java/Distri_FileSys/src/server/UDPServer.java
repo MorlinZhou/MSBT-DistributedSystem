67a7f4edd7888c7d6d9d5937e5b35b50e56ec6a0
K <1206109134@qq.com>
2023-09-27 22:28:42 +0800

    some changes

--- Java/Distri_FileSys/src/server/UDPServer.java
+++ Java/Distri_FileSys/src/server/UDPServer.java
@@ -1,63 +1,42 @@
 package server;
-import api.callModel;
+
 import java.net.*;
 import java.io.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Properties;
 
 
 public class UDPServer {
-    public static void openServer() throws IOException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
-//        DatagramSocket aSocket=null;
-        ServerSocket serverSocket = new ServerSocket(8888);
+    public static void main(String args[]) throws IOException {
+        DatagramSocket socket=null;
         System.out.println("服务开启...");
-//            aSocket=new DatagramSocket(8088);
-//            byte[] buffer=new byte[100];
+        try{
+            socket=new DatagramSocket(8088);
+            byte[] receiveData = new byte[1024];
             while(true){
-                Socket socket = serverSocket.accept();
-                System.out.println("连接成功："+socket.getLocalAddress());
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                // 接收客户端发送的数据包
+                socket.receive(receivePacket);
 
-//           接受对象
-                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-                callModel netModel = (callModel) in.readObject();
-                String className = netModel.getClassName();
-                String methodName = netModel.getMethodName();
-                Object[] args = netModel.getArgs();             //参数值
-                Class[] types = new Class[args.length];       //参数类型
-                for (int i = 0; i < types.length; i++){
-                    types[i] = args[i].getClass();
-                }
-                String classNameServer = getPropertyValue(netModel.getClassName());
-                Class clazz = Class.forName(classNameServer);
-                Method method = clazz.getMethod(netModel.getMethodName(), types);
-//            调用方法
-                Object resObj = method.invoke(clazz.newInstance(), args);
-//            发送对象
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                out.writeObject(resObj);
-                out.flush();
-//            关闭流及Socket
-                out.close();
-                in.close();
+                // 从数据包中提取客户端的消息
+                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Received from client: " + clientMessage);
+                // 构建回应消息
+                String responseMessage = "Hello, Client!";
+                byte[] responseData = responseMessage.getBytes();
+
+                // 获取客户端的地址和端口信息
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+
+                // 创建回应数据包并发送给客户端
+                DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
+                socket.send(responsePacket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null && !socket.isClosed()) {
                 socket.close();
             }
-    }
-
-    private static String getPropertyValue(String key) throws IOException {
-        Properties pro = new Properties();
-        FileInputStream in = new FileInputStream("../config.properties");
-        pro.load(in);
-        in.close();
-        return pro.getProperty(key);
-    }
-
-    public static void main(String[] args){
-        try {
-            openServer();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
