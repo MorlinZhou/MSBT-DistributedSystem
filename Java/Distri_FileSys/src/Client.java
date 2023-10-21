067486873d5a3125c382e8b5caca2e90ec1853f6
K <1206109134@qq.com>
2023-10-21 14:38:06 +0800

    实现文件读写和monitoring

---
+++ Java/Distri_FileSys/src/Client.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client {
+    private DatagramSocket socket;
+    private InetAddress serverAddress;
+    private static final int PORT = 8080;
+    private static final int BUFFER_SIZE = 1024;
+
+    public Client() throws SocketException, UnknownHostException {
+        socket = new DatagramSocket();
+        serverAddress = InetAddress.getByName("localhost");
+    }
+
+    public String readFile(String filePath, int offset, int byteCount) throws IOException {
+        String request = "READ:" + filePath + ":" + offset + ":" + byteCount;
+        byte[] sendBuffer = request.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, PORT);
+        socket.send(sendPacket);
+
+        byte[] receiveBuffer = new byte[BUFFER_SIZE];
+        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        socket.receive(receivePacket);
+        return new String(receivePacket.getData(), 0, receivePacket.getLength());
+    }
+
+    public String writeToFile(String filePath, int offset, String byteSequence) throws IOException {
+        String request = "WRITE:" + filePath + ":" + offset + ":" + byteSequence;
+        byte[] sendBuffer = request.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, PORT);
+        socket.send(sendPacket);
+
+        byte[] receiveBuffer = new byte[BUFFER_SIZE];
+        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        socket.receive(receivePacket);
+        return new String(receivePacket.getData(), 0, receivePacket.getLength());
+    }
+
+    public String monitorFile(String filePath, long interval) throws IOException {
+        String request = "MONITOR:" + filePath + ":" + interval;
+        byte[] sendBuffer = request.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, PORT);
+        socket.send(sendPacket);
+
+        byte[] receiveBuffer = new byte[BUFFER_SIZE];
+        DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+        socket.receive(receivePacket);
+        String response = new String(receivePacket.getData(), 0, receivePacket.getLength());
+
+        if (response.startsWith("SUCCESS")) {
+            while (true) { // Continuously monitor for updates until the interval expires
+                socket.receive(receivePacket);
+                System.out.println("Received update: " + new String(receivePacket.getData(), 0, receivePacket.getLength()));
+            }
+        }
+        return response;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Client client = new Client();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Choose operation (READ/WRITE/MONITOR):");
+            String operation = scanner.nextLine().toUpperCase();
+            if ("READ".equals(operation)) {
+                System.out.println("Enter file path:");
+                String filePath = scanner.nextLine();
+                System.out.println("Enter offset:");
+                int offset = scanner.nextInt();
+                System.out.println("Enter byte count:");
+                int byteCount = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
+
+                String response = client.readFile(filePath, offset, byteCount);
+                System.out.println(response);
+            }
+            else if ("WRITE".equals(operation)) {
+                System.out.println("Enter file path:");
+                String filePath = scanner.nextLine();
+                System.out.println("Enter offset:");
+                int offset = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
+                System.out.println("Enter byte sequence:");
+                String byteSequence = scanner.nextLine();
+
+                String response = client.writeToFile(filePath, offset, byteSequence);
+                System.out.println(response);
+            } else if ("MONITOR".equals(operation)) {
+                System.out.println("Enter file path:");
+                String filePath = scanner.nextLine();
+                System.out.println("Enter monitoring interval (in milliseconds):");
+                long interval = scanner.nextLong();
+                scanner.nextLine(); // Consume newline
+
+                String response = client.monitorFile(filePath, interval);
+                System.out.println(response);
+            }else {
+                System.out.println("Invalid operation. Please choose READ or WRITE.");
+            }
+        }
+    }
+}
--- Java/Distri_FileSys/src/server/UDPServer.java
+++ Java/Distri_FileSys/src/server/UDPServer.java
@@ -1,59 +1,170 @@
 package server;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.*;
 import java.io.*;
-
+import java.net.*;
+import java.util.*;
 
 public class UDPServer {
-    public static void main(String args[]) throws IOException {
-        System.out.println("服务开启...");
-        try{
-            ServerSocket server = new ServerSocket(8080);
-            System.out.println("Server is running...");
-//            byte[] receiveData = new byte[1024];
-            while(true){
-                Socket socket = server.accept();
-//                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                // 接收客户端发送的数据包
-                ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-                ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-                // 读取接口名
-                String interfaceName = input.readUTF();
-                // 读取方法名
-                String methodName = input.readUTF();
-                // 读取方法参数类型
-                Class<?>[] parameterTypes = (Class<?>[]) input.readObject();
-                // 读取方法参数值
-                Object[] arguments = (Object[]) input.readObject();
+    private DatagramSocket socket;
+    private static final int PORT = 8080;
+    private static final int BUFFER_SIZE = 1024;
+    private Map<String, List<RegisteredClient>> registeredClients = new HashMap<>();
 
-                Class<?> serviceInterfaceClass = Class.forName(interfaceName);
-                Object service = new ServiceImpl();
-                Method method = serviceInterfaceClass.getMethod(methodName, parameterTypes);
-                Object result = method.invoke(service, arguments);
+    public UDPServer() throws SocketException {
+        socket = new DatagramSocket(PORT);
+    }
 
-                output.writeObject(result);
+    private static class RegisteredClient {
+        InetAddress address;
+        int port;
+        long endTime;
 
-                /*// 从数据包中提取客户端的消息
-                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                System.out.println("Received from client: " + clientMessage);
-                // 构建回应消息
-                String responseMessage = "Hello, Client!";
-                byte[] responseData = responseMessage.getBytes();
-
-                // 获取客户端的地址和端口信息
-                InetAddress clientAddress = receivePacket.getAddress();
-                int clientPort = receivePacket.getPort();
-
-                // 创建回应数据包并发送给客户端
-                DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, clientAddress, clientPort);
-                socket.send(responsePacket);*/
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-            throw new RuntimeException(e);
+        RegisteredClient(InetAddress address, int port, long interval) {
+            this.address = address;
+            this.port = port;
+            this.endTime = System.currentTimeMillis() + interval;
         }
+    }
+
+
+    public void start() throws IOException {
+        System.out.println("UDPServer online...");
+        byte[] receiveBuffer = new byte[BUFFER_SIZE];
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            socket.receive(receivePacket);
+            String request = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            String response = handleRequest(request, receivePacket.getAddress(), receivePacket.getPort());
+            byte[] sendBuffer = response.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, receivePacket.getAddress(), receivePacket.getPort());
+            socket.send(sendPacket);
+        }
+    }
+
+    private String handleRequest(String request, InetAddress address, int port) {
+        String[] parts = request.split(":");
+        String command = parts[0];
+//        if (parts.length != 4 || !parts[0].equals("READ")) {
+//            return "ERROR:Invalid request format";
+//        }
+        if ("READ".equals(command)) {
+            String filePath = parts[1];
+            int offset;
+            int byteCount;
+            try {
+                offset = Integer.parseInt(parts[2]);
+                byteCount = Integer.parseInt(parts[3]);
+            } catch (NumberFormatException e) {
+                return "ERROR:Invalid offset or byte count";
+            }
+            try (RandomAccessFile file = new RandomAccessFile(filePath, "r")) {
+                if (offset >= file.length()) {
+                    return "ERROR:Offset exceeds file length";
+                }
+                byte[] buffer = new byte[byteCount];
+                file.seek(offset);
+                int bytesRead = file.read(buffer, 0, byteCount);
+
+                return "CONTENT:" + new String(buffer, 0, bytesRead);
+            } catch (FileNotFoundException e) {
+                return "ERROR:File not found";
+            } catch (IOException e) {
+                return "ERROR:" + e.getMessage();
+            }
+        } else if ("WRITE".equals(command)) {
+            String response = handleWriteRequest(parts);
+            notifyRegisteredClients(parts[1], parts[3]); // notify about the written content
+            return response;
+        } else if ("MONITOR".equals(command)) {
+            return handleMonitorRequest(parts,address, port);
+        }else {
+            return "ERROR:Invalid request format";
+        }
+    }
+
+    private String handleWriteRequest(String[] parts) {
+        if (parts.length != 4) {
+            return "ERROR:Invalid request format";
+        }
+
+        String filePath = parts[1];
+        int offset;
+        try {
+            offset = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException e) {
+            return "ERROR:Invalid offset";
+        }
+        String byteSequence = parts[3];
+
+        try (RandomAccessFile file = new RandomAccessFile(filePath, "rw")) {
+            if (offset > file.length()) {
+                return "ERROR:Offset exceeds file length";
+            }
+
+            byte[] restOfFile = new byte[(int) (file.length() - offset)];
+            file.seek(offset);
+            file.readFully(restOfFile);
+            file.seek(offset);
+            file.write(byteSequence.getBytes());
+            file.write(restOfFile);
+
+            return "SUCCESS:Content written successfully";
+        } catch (FileNotFoundException e) {
+            return "ERROR:File not found";
+        } catch (IOException e) {
+            return "ERROR:" + e.getMessage();
+        }
+    }
+
+    private String handleMonitorRequest(String[] parts, InetAddress address, int port) {
+        if (parts.length != 3) {
+            return "ERROR:Invalid request format";
+        }
+
+        String filePath = parts[1];
+        long interval;
+        try {
+            interval = Long.parseLong(parts[2]);
+        } catch (NumberFormatException e) {
+            return "ERROR:Invalid interval";
+        }
+
+        if (!registeredClients.containsKey(filePath)) {
+            registeredClients.put(filePath, new ArrayList<>());
+        }
+        registeredClients.get(filePath).add(new RegisteredClient(address, port, interval));
+
+        // Schedule cleanup task to remove client registration after the interval
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                registeredClients.get(filePath).removeIf(client -> client.endTime <= System.currentTimeMillis());
+            }
+        }, interval);
+
+        return "SUCCESS:Monitoring started for " + filePath + " for " + interval + " milliseconds";
+    }
+
+    private void notifyRegisteredClients(String filePath, String content) {
+        List<RegisteredClient> clients = registeredClients.get(filePath);
+        if (clients == null) return;
+
+        byte[] sendBuffer = ("UPDATE:" + filePath + ":" + content).getBytes();
+        for (RegisteredClient client : clients) {
+            if (client.endTime > System.currentTimeMillis()) {
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, client.address, client.port);
+                try {
+                    socket.send(sendPacket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        UDPServer server = new UDPServer();
+        server.start();
     }
 }
