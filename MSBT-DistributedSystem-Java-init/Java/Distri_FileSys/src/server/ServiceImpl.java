c4398913b1a0f8e1a590737080d8d8efc9d3e297
Cylconee11 <144906617+Cylconee11@users.noreply.github.com>
2023-10-22 16:20:34 +0800

    Delete MSBT-DistributedSystem-Java-init directory

--- MSBT-DistributedSystem-Java-init/Java/Distri_FileSys/src/server/ServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package server;
-import api.PublicService;
-
-public class ServiceImpl implements PublicService{
-    @Override
-    public String sayHello(String name) {
-        return "Hello, " + name + "!";
-    }
-}
